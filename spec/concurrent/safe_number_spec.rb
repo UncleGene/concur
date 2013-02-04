148df9901ea76391178642ae4193ca907b86464c
Eugene Kalenkovich <rubify@softover.com>
2013-02-04 09:45:43 -0800

    Safe number test fix

--- spec/concurrent/safe_number_spec.rb
+++ spec/concurrent/safe_number_spec.rb
@@ -7,21 +7,21 @@ describe SafeNumber do
 
   it 'should have unique values for find_or_create' do
     concurrently do
-      SafeNumber.find_or_create(SafeNumber.count).new_record?.wont_equal true #Find better construct?
+      assert !SafeNumber.find_or_create(SafeNumber.count).new_record?
     end
     SafeNumber.count.must_equal SafeNumber.select('distinct value').count
   end
 
   it 'should have unique values for safe_create' do
     concurrently do
-      SafeNumber.safe_create(:value => SafeNumber.count).new_record?.wont_equal true
+      SafeNumber.safe_create(:value => SafeNumber.count)
     end
     SafeNumber.count.must_equal SafeNumber.select('distinct value').count
   end
 
   it 'should have unique values for safe_find_or_create' do
     concurrently do
-      SafeNumber.safe_find_or_create(:value => SafeNumber.count).new_record?.wont_equal true
+      assert !SafeNumber.safe_find_or_create(:value => SafeNumber.count).new_record?
     end
     SafeNumber.count.must_equal SafeNumber.select('distinct value').count
   end
